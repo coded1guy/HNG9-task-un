c6c9280cd45544e091918a48a0182e3e20dfa6b5
coded1guy <grggmmnl@gmail.com>
2022-11-05 02:33:57 +0100

    feat: installed react router and created a basic structure for the route.

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./css/contact.css";
+import Footer from "../components/Footer";
+
+const Contact = () => {
+  return (
+    <div id="contact-page">
+      <header></header>
+      <main>
+        <form></form>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+export default Contact;
