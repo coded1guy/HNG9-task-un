abaa5b400536756606c8cb924118e978f75b9546
coded1guy <grggmmnl@gmail.com>
2022-11-06 14:00:24 +0100

    feat: add the profile app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
